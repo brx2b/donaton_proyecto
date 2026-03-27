18b74d99e9f530c648898d7a0c00365e38e9c972
Brx2b <briansnowshine@gmail.com>
2026-03-27 16:12:12 -0300

    correcion y cambios de servicios para que sea en eureka

---
+++ api-necesidades/src/main/java/com/necesidades/api_necesidades/ApiNecesidadesApplication.java
@@ -0,0 +1,13 @@
+package com.necesidades.api_necesidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNecesidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNecesidadesApplication.class, args);
+	}
+
+}
